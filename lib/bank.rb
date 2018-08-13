0c1a329a2faba13e7596b19fd672f1ffc7103720
Layth <laythq@gmail.com>
2018-08-13 11:07:40 +0100

    Initialized Bank class, wrote method to show user options and passed first test

---
+++ lib/bank.rb
@@ -0,0 +1,9 @@
+class Bank
+
+  def show_options
+    puts "1. Show Balance"
+    puts "2. Deposit"
+    puts "3. Withdrawal"
+  end
+
+end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,6 +2,6 @@ require 'bank'
 
 describe Bank do
   it 'should print a list of options to the console' do
-    expect { show_options }.to output("1. Show Balance 2. Deposit 3. Withdraw").to_stdout
+    expect { subject.show_options }.to output("1. Show Balance\n2. Deposit\n3. Withdrawal\n").to_stdout
   end
 end
