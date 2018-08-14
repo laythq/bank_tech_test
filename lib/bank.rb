d6a061b617a0b186504771a3af81a7ba0e168035
Layth <laythq@gmail.com>
2018-08-14 11:56:06 +0100

    Added confirmation messages to each statement

--- lib/bank.rb
+++ lib/bank.rb
@@ -15,12 +15,19 @@ class Bank
     @balance += amount
     new_deposit = Transaction.new(amount, 0, @balance)
     push_to_activity(new_deposit)
+    return "Deposit confirmed"
   end
 
   def withdraw(amount)
     @balance -= amount
     new_withdrawal = Transaction.new(0, amount, @balance)
     push_to_activity(new_withdrawal)
+    return "Withdrawal confirmed"
+  end
+
+  def show_statement
+    Statement.new(@activity)
+    return "End of Statement"
   end
 
   def push_to_activity(object)
