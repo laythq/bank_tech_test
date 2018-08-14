b7003748a20a1d0c22b5fa4b3a7763234afd7912
Layth <laythq@gmail.com>
2018-08-14 19:24:27 +0100

    Modified statement_spec #bank_statement test description

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -4,6 +4,7 @@ describe View do
   before(:each) do
     @view = View.new
   end
+  
   describe '#show_options' do
     it 'should print a list of options to the console' do
       expect { @view.show_options }.to output("1. Bank Statement\n2. Deposit\n3. Withdrawal\n4. Exit\n").to_stdout
