dc1e0361e5e49ea8a058ffa5e45a6c7b84b83359
Layth <laythq@gmail.com>
2018-08-14 10:45:48 +0100

    Extracted #command, #show_options and #interactive_menu methods to App class

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,58 @@
 require_relative 'bank'
 
-bank = Bank.new
-bank.interactive_menu
+
+class App
+
+  def initialize
+    @bank = Bank.new
+  end
+
+  def deposit(stdin = $stdin)
+    puts "How much would you like to deposit?"
+    amount = stdin.gets.chomp
+    @bank.balance += amount.to_f
+    new_deposit = Deposit.new(amount)
+    push_to_activity(new_deposit)
+  end
+
+  def withdraw(stdin = $stdin)
+    puts "How much would you like to withdraw?"
+    amount = stdin.gets.chomp
+    @bank.balance -= amount.to_f
+    new_withdrawal = Withdrawal.new(amount)
+    push_to_activity(new_withdrawal)
+  end
+
+  def push_to_activity(object)
+    @bank.activity.push(object)
+  end
+
+  def show_options
+    puts "1. Bank Statement"
+    puts "2. Deposit"
+    puts "3. Withdrawal"
+    puts "4. Exit"
+  end
+
+  def command(stdin = $stdin)
+    input = stdin.gets.chomp
+    case input
+      when "1"
+        Statement.new(@bank.balance, @bank.activity)
+      when "test"
+        p "test"
+      when "2"
+        deposit
+      when "3"
+        withdraw
+      when "4"
+        exit
+      else
+        p "Please select a valid option"
+    end
+  end
+
+end
+
+app = App.new
+app.interactive_menu
--- lib/bank.rb
+++ lib/bank.rb
@@ -12,31 +12,6 @@ class Bank
     @activity = []
   end
 
-  def show_options
-    puts "1. Bank Statement"
-    puts "2. Deposit"
-    puts "3. Withdrawal"
-    puts "4. Exit"
-  end
-
-  def command(stdin = $stdin)
-    input = stdin.gets.chomp
-    case input
-      when "1"
-        Statement.new(@balance, @activity)
-      when "test"
-        p "test"
-      when "2"
-        deposit
-      when "3"
-        withdraw
-      when "4"
-        exit
-      else
-        p "Please select a valid option"
-    end
-  end
-
   def deposit(stdin = $stdin)
     puts "How much would you like to deposit?"
     amount = stdin.gets.chomp
